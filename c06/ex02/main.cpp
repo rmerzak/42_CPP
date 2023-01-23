7e6f7754c8d986005290eb37df43876a3c7d2659
rmerzak <rmerzak@student.1337.ma>
2023-01-23 20:28:57 +0100

    fixing error ex02 day06

--- c06/ex02/main.cpp
+++ c06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rmerzak <rmerzak@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 18:01:14 by rmerzak           #+#    #+#             */
-/*   Updated: 2023/01/22 18:27:06 by rmerzak          ###   ########.fr       */
+/*   Updated: 2023/01/23 20:27:21 by rmerzak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 
 #include <cstdlib>
 #include <ctime>
+#include <typeinfo>
 #include <iostream>
 
 Base * generate(void) {
@@ -53,14 +54,33 @@ void identify(Base* p) {
 
 
 void identify(Base& p) {
-     if (dynamic_cast<A*>(&p)) {
+    try
+    {
+        A &a = dynamic_cast<A&>(p);
+        (void)a;
         std::cout << "A" << std::endl;
     }
-    else if (dynamic_cast<B*>(&p)) {
-        std::cout << "B" << std::endl;
-    }
-    else if (dynamic_cast<C*>(&p)) {
-        std::cout << "C" << std::endl;
+    catch(std::bad_cast const&)
+    {
+        try
+        {
+            B &b = dynamic_cast<B&>(p);
+            (void)b;
+            std::cout << "B" << std::endl;
+        }
+        catch(std::bad_cast const&)
+        {
+            try
+            {
+                C &c = dynamic_cast<C&>(p);
+                (void)c;
+                std::cout << "C" << std::endl;
+            }
+            catch(std::bad_cast const&)
+            {
+                std::cerr << "error cast "<< '\n';
+            }
+        }
     }
 }
 
