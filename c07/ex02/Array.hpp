36162a71b1a8e8dc58e3dd3ab6d2a646feb6b189
rmerzak <rmerzak@student.1337.ma>
2023-01-24 23:45:50 +0100

    push

--- c07/ex02/Array.hpp
+++ c07/ex02/Array.hpp
@@ -1,1 +1,63 @@
- 
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmerzak <rmerzak@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/24 22:52:43 by rmerzak           #+#    #+#             */
+/*   Updated: 2023/01/24 23:26:49 by rmerzak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+template <class T>
+class Array {
+  private:
+  T *data;
+  unsigned int d_size;
+  public:
+  Array():data(NULL) ,d_size(0) {};
+  Array(unsigned int n):data(new T[n]()), d_size(n) {};
+  ~Array() {
+    if (data) {
+        delete [] data;
+    }
+  };
+  Array(const Array<T> &d):data(d.data),d_size(d.d_size) {
+    for (unsigned int i = 0; i<d_size;++i) {
+        this->data[i] = d.data[i];
+    }
+  };
+  Array<T>&operator=(const Array<T> &d) {
+    if (this != &d) {
+        if (data) {
+            delete [] data;
+        }
+        this->d_size = d.d_size;
+        this->data = new T[d_size];
+        for (unsigned int i = 0; i<d_size;++i) {
+            this->data[i] = d.data[i];
+        }
+    }
+    return *this;
+  };
+  class OutOfRange : public std::exception {
+     virtual const char* what() const throw() {
+        return "Array index out of bounds!";
+    }
+  };
+
+  T& operator[](unsigned int index) {
+        if (index >= d_size) {
+            throw Array::OutOfRange();
+        }
+        return data[index];
+    }
+  unsigned int size(void) {
+    return this->d_size;
+  };
+};
+#endif
