e9a46da5953da7c75ca09bd33cedf760a0e6948f
rmerzak <rmerzak@student.1337.ma>
2023-01-26 19:17:18 +0100

    push

--- c07/ex01/iter.hpp
+++ c07/ex01/iter.hpp
@@ -6,16 +6,16 @@
 /*   By: rmerzak <rmerzak@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 19:33:35 by rmerzak           #+#    #+#             */
-/*   Updated: 2023/01/24 20:45:24 by rmerzak          ###   ########.fr       */
+/*   Updated: 2023/01/26 19:14:25 by rmerzak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 #define ITER_HPP
 
-template <typename T> void iter(T* arr, int length, void (*func)(T*)) {
-    for (int i = 0; i < length; i++)
-        func(&arr[i]);
+template <typename T> void iter(T* arr, size_t length, void (*func)(T const &)) {
+    for (size_t i = 0; i < length; i++)
+        func(arr[i]);
 }
 
 #endif
--- c07/ex01/main.cpp
+++ c07/ex01/main.cpp
@@ -1,29 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmerzak <rmerzak@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 19:13:43 by rmerzak           #+#    #+#             */
+/*   Updated: 2023/01/26 19:13:44 by rmerzak          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
 #include "iter.hpp"
 
-
-void printInt(char *i) {
-    std::cout << *i;
-}
-
-void print(int *i) {
-    std::cout << *i;
-}
-
-void add(char *i) {
-    *i = *i + 1;
-}
-void addi(int *i) {
-    *i = *i + 1;
-}
-
-int main(int, char**)
+class Awesome
 {
-   int i[6] = {1,2,3,4,5};
-   char str[15] = "hello";
-   iter(str,6,add);
-   iter(i,6,addi);
-   iter(str,6,printInt);
-   iter(i,5,print);
-    return 0;
+  public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+  private:
+    int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs ) { o << rhs.get(); return o; }
+
+template< typename T >
+void print( T const & x ) { std::cout << x << std::endl; }
+
+int main() {
+  int tab[] = { 0, 1, 2, 3, 4 };  // <--- I never understood why you can't write int[] tab. Wouldn't that make more sense?
+  Awesome tab2[5];
+
+  iter( tab, 5, print);
+  iter( tab2, 5, print<Awesome> );
+
+  return 0;
 }
